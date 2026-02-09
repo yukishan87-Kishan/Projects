bb7d209eb7b0e00d54b81f70d95860e3b24beab5
Kishan Y U <yukishan87@gmail.com>
2026-02-09 12:18:36 +0530

    final grade calculation

--- project2.cpp
+++ project2.cpp
@@ -5,17 +5,22 @@ class findGrade
 {
     float q1,q2,qT,midMarks,finalmarks;
     float percentage ,percQ,percM ,percF;
+    string grade;
     public:
         void getMarks();
         void calcMarks();
         void calcGrade();
         void showGrade();
         
-};
+}obj;
 
 int main()
 {
-        
+        obj.getMarks();
+        obj.calcMarks();
+        obj.calcGrade();
+        obj.showGrade();
+
     return 0;
 }
 
@@ -32,12 +37,54 @@ void findGrade :: getMarks()
     cout<<endl;
 }
 
-void findGrade :: calcGrade()
+void findGrade :: calcMarks()
 {
     qT=q1+q2;
     percQ=(25.0/20.0)*qT;
-    percM=(25.0/100.0)*qT;
-    cout<<"\nEnter the marks of Final Term out of(100) : ";
-    cin>>finalmarks;
+    percM=(25.0/100.0)*midMarks;
+    percF=(50.0/100.0)*midMarks;
+
+    percentage=percQ+percM+percF;
+
+}
+
+void findGrade ::calcGrade()
+{
+    if(percentage>= 90.0)
+    {
+        grade="A+";
+    }
+    if(percentage>= 80.0 && percentage<90.0)
+    {
+        grade="A";
+    }
+    if(percentage>= 70.0 && percentage<80.0)
+    {
+        grade="B+";
+    }
+    if(percentage>= 60.0 && percentage<70.0)
+    {
+        grade="B";
+    }
+    if(percentage>= 50.0 && percentage<60.0)
+    {
+        grade="C+";
+    }
+    if(percentage>= 40.0 && percentage<50.0)
+    {
+        grade="C";
+    }
+    if(percentage < 40.0)
+    {
+        grade="FAIL";
+    }
+}
+
+void findGrade ::showGrade()
+{
+    cout<<"\n";
+    cout<<"======================================="<<endl;
+    cout<<"The Grade Obtained is : "<<grade<<endl;
+    cout<<"======================================="<<endl;
     cout<<endl;
 }
